be808007a5052c9051202942872d34cafe7b85f8
Johannes <johtso@gmail.com>
2013-04-14 16:18:17 +0100

    Make test data more readable

--- 2013/qualification/b/test.py
+++ 2013/qualification/b/test.py
@@ -4,17 +4,24 @@ import b
 
 
 @pytest.mark.parametrize(('pattern', 'expected'), [
-    ([[2, 1, 2],
-      [1, 1, 1],
-      [2, 1, 2]], True),
-
-    ([[2, 2, 2, 2, 2],
-      [2, 1, 1, 1, 2],
-      [2, 1, 2, 1, 2],
-      [2, 1, 1, 1, 2],
-      [2, 2, 2, 2, 2]], False),
-
-    ([[1, 2, 1]], True),
+    (
+        [[2, 1, 2],
+         [1, 1, 1],
+         [2, 1, 2]],
+        True
+    ),
+    (
+        [[2, 2, 2, 2, 2],
+         [2, 1, 1, 1, 2],
+         [2, 1, 2, 1, 2],
+         [2, 1, 1, 1, 2],
+         [2, 2, 2, 2, 2]],
+        False
+    ),
+    (
+        [[1, 2, 1]],
+        True
+    ),
 ])
 def test_possible(pattern, expected):
     assert b.possible(pattern) == expected
--- 2013/qualification/d/test.py
+++ 2013/qualification/d/test.py
@@ -4,14 +4,32 @@ import d
 
 
 @pytest.mark.parametrize(('starting_keys', 'chests', 'expected'), [
-    ([1], [(1, []),
-           (1, [1, 3]),
-           (2, []),
-           (3, [2])], [2, 1, 4, 3]),
-    ([1, 1, 1], [(1, []),
-                 (1, []),
-                 (1, [])], [1, 2, 3]),
-    ([2], [(1, [1])], None)
+    (
+        [1],
+        [
+            (1, []),
+            (1, [1, 3]),
+            (2, []),
+            (3, [2]),
+        ],
+        [2, 1, 4, 3]
+    ),
+    (
+        [1, 1, 1],
+        [
+            (1, []),
+            (1, []),
+            (1, []),
+        ],
+        [1, 2, 3]
+    ),
+    (
+        [2],
+        [
+            (1, [1]),
+        ],
+        None
+    )
 ])
 def test_solve(starting_keys, chests, expected):
     result = d.solve(starting_keys, chests)
